5339df9bb9452e112b49a64f78e4bfdc58487475
bamboo <nmz89825@gmail.com>
2024-11-04 14:01:02 +0800

    fix: social media footer style.

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -17,13 +17,15 @@ const SocialArrow = ({ name }: { name: string }) => {
 
 export default function SocialMedia() {
   return (
-    <div className="min-h-screen pt-20">
+    <div className="min-h-screen pt-20 px-8 sm:px-0">
       <Footer>
-        <div className="flex flex-row justify-between mb-16">
-          <div className="font-title text-5.5xl max-w-52">Social Media</div>
-          <div className="w-3/5 flex flex-col gap-8">
-            <div className="flex flex-row gap-8">
-              <div className="h-50 w-7/12 bg-white rounded-2.5xl border border-dark-green p-6 relative cursor-pointer transition-all hover:shadow-[0_4px_0_0_#000]">
+        <div className="flex flex-col sm:flex-row justify-between mb-16">
+          <div className="font-title text-2xl sm:text-5.5xl w-full sm:max-w-52 mb-6">
+            Social Media
+          </div>
+          <div className="w-full sm:w-3/5 flex flex-col gap-6 sm:gap-8">
+            <div className="flex flex-col sm:flex-row gap-6 sm:gap-8">
+              <div className="h-50 w-full sm:w-7/12 bg-white rounded-2.5xl border border-dark-green p-6 relative cursor-pointer transition-all hover:shadow-[0_4px_0_0_#000]">
                 <X className="w-11 h-10" />
                 <SocialArrow name="X" />
               </div>
@@ -32,12 +34,12 @@ export default function SocialMedia() {
                 <SocialArrow name="Discord" />
               </div>
             </div>
-            <div className="flex flex-row gap-8">
+            <div className="flex flex-col sm:flex-row gap-6 sm:gap-8">
               <div className="grow h-50 bg-[#e3fbff] rounded-2.5xl border border-dark-green p-6 relative cursor-pointer transition-all hover:shadow-[0_4px_0_0_#000]">
                 <Telegram className="w-12 h-12" />
                 <SocialArrow name="Telegram" />
               </div>
-              <div className="h-50 w-7/12 bg-lime-green rounded-2.5xl border border-dark-green p-6 relative cursor-pointer transition-all hover:shadow-[0_4px_0_0_#000]">
+              <div className="h-50 w-full sm:w-7/12 bg-lime-green rounded-2.5xl border border-dark-green p-6 relative cursor-pointer transition-all hover:shadow-[0_4px_0_0_#000]">
                 <Medium className="w-12 h-12" />
                 <SocialArrow name="Medium" />
               </div>
