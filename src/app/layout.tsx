a85592b390904fb5eac50e7cc0f3d3c7d5a967ca
Meathill <meathill@gmail.com>
2024-11-21 14:01:56 +0800

    bug: fixed link in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
     <html lang="en">
     <head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
       <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
             rel="stylesheet"/>
     </head>
